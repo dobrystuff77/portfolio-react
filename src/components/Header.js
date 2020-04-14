f2f6918643dea0b77706c99d40fc5f26bcdc2728
Adrian Wysocki <dobrystuff77@gmail.com>
2020-04-14 23:05:40 +0200

    home section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,41 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { Typography, Avatar, Grid, Box } from "@material-ui/core";
 import avatar from "../avatar.png";
+import Typed from "react-typed";
+
+// CSS styles
+const useStyles = makeStyles(theme => ({
+  avatar: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+    margin: theme.spacing(1)
+  },
+  title: { color: "tomato" },
+  subtitle: { color: "tan", marginBottom: "3rem" }
+}));
 
 const Header = () => {
+  const classes = useStyles();
+  console.log(classes.avatar);
   return (
     <Box>
-      <Avatar src={avatar} alt="Adrian Wysocki" />
+      <Avatar className={classes.avatar} src={avatar} alt="Adrian Wysocki" />
+      <Typography className={classes.title} variant="h4">
+        <Typed strings={["Adrian Wysocki"]} typeSpeed={20} />
+      </Typography>
+      <br />
+      <Typography className={classes.subtitle} variant="h5">
+        <Typed
+          strings={[
+            "Junior Full Stack Web Developer",
+            "HTML, CSS, jQuery, Java Script, React, express, handlebars"
+          ]}
+          typeSpeed={80}
+          backSpeed={90}
+          loop
+        />
+      </Typography>
     </Box>
   );
 };
