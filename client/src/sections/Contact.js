242c0dd3e412334bc5ed6ad3a43ea428e7d20a6e
Adrian Wysocki <dobrystuff77@gmail.com>
2020-04-27 23:45:04 +0200

    backed of contact section

--- client/src/sections/Contact.js
+++ client/src/sections/Contact.js
@@ -11,19 +11,31 @@ class Contact extends Component {
     this.setState({ [e.target.name]: e.target.value });
   }
 
+  closeEmpty() {
+    console.log("this.state.emptyFields: ", this.state.emptyFields);
+    this.setState({ emptyFields: false });
+  }
+
   submit() {
+    this.setState({ emptyFields: false });
     console.log("first", this.state.name);
     console.log("email", this.state.email);
+    console.log("message", this.state.subject);
     console.log("message", this.state.message);
 
     axios
       .post("/api/message", {
         name: this.state.name,
         email: this.state.email,
+        subject: this.state.subject,
         message: this.state.message
       })
       .then(data => {
-        console.log("res from axios POST/api/message", data);
+        console.log("res from axios POST/api/message", data.data.data);
+        if (data.data.data === false) {
+          console.log("empty field!!");
+          this.setState({ emptyFields: true });
+        }
       })
       .catch(err => {
         console.log(err);
@@ -33,7 +45,11 @@ class Contact extends Component {
   render() {
     return (
       <div className="contact-section" id="contact">
-        {this.state.emptyFields && <>EMPTY FIELDS</>}
+        {this.state.emptyFields && (
+          <>
+            EMPTY FIELDS<div onClick={() => this.closeEmpty()}>X</div>
+          </>
+        )}
         <input
           name="name"
           onChange={e => this.handleChange(e)}
@@ -44,6 +60,12 @@ class Contact extends Component {
           name="email"
           onChange={e => this.handleChange(e)}
           placeholder="your email"
+        />
+        <br />
+        <input
+          name="subject"
+          onChange={e => this.handleChange(e)}
+          placeholder="subject"
         />
         <br />
         <textarea
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const port = 5000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
+const ses = require("./ses");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -13,12 +15,29 @@ app.post("/api/message", (req, res) => {
   if (
     req.body.name == undefined ||
     req.body.email == undefined ||
+    req.body.subject == undefined ||
     req.body.message == undefined
   ) {
     console.log("empty message");
     res.json({ data: false });
   } else {
-    res.json({ data: true });
+    let name = req.body.name;
+    let email = req.body.email;
+    let message = req.body.message;
+    let subject = req.body.subject;
+
+    let messageContent = name + " <" + email + "> " + message;
+
+    ses
+      .sendEmail("dobrystuff77@gmail.com", messageContent, subject)
+      .then(result => {
+        console.log("result from sendEmail:", result);
+        res.json({ data: true });
+      })
+      .catch(err => {
+        console.log("err in sendEmail: ", err);
+        res.json({ data: false });
+      });
   }
 });
 
