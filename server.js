777858f9c94769648f7e883cd4e2fceae77d58f4
Adrian Wysocki <dobrystuff77@gmail.com>
2020-04-29 00:11:42 +0200

    deploying to heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-
+const path = require("path");
 const ses = require("./ses");
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -40,6 +40,15 @@ app.post("/api/message", (req, res) => {
       });
   }
 });
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(process.env.PORT || port, () =>
   console.log(`Server started on port ${port}`)
