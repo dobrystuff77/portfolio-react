fa7062f73408a5c59e85036f785413a1db6f28a3
Adrian Wysocki <dobrystuff77@gmail.com>
2020-07-24 15:22:34 +0200

    fixing bugs

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,21 @@ import Contact from "./sections/Contact";
 import Main from "./sections/Main";
 import Projects from "./sections/Projects";
 
+import data from "./sections/data.json";
+
 class App extends Component {
   state = {};
+
+  componentDidMount() {
+    // console.log("!!!", data);
+    // console.log(data.projects.length);
+    // for (let i = 0; i < data.projects.length; i++) {
+    //   console.log(data.projects[i].live_page);
+    //   fetch(data.projects[i].live_page);
+    // }
+    fetch("http://www.kanten-meer.herokuapp.com");
+  }
+
   render() {
     return (
       <>
